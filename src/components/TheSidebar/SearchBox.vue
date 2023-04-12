552ca3a0fdf3649dd8821aa7cfc61858d877b2d1
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-13 02:14:52 +0300

    search box

---
+++ src/components/TheSidebar/SearchBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="pl-5 pr-5">
+    <div
+      class="flex items-center rounded-md bg-light-primary-color mt-2 py-1 border-light-text-color duration-100"
+      :class="{ border: isMenuOpen, SearchPaddingInClose }"
+    >
+      <IconSearch
+        class="block float-left cursor-pointer ml-1"
+        :class="SearchIconMarginInClose"
+      />
+      <input
+        type="search"
+        name=""
+        id=""
+        placeholder="Search"
+        class="text-base bg-transparent w-full text-light-text-color focus:outline-none"
+        :class="{ hidden: !isMenuOpen }"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import IconSearch from "../icons/IconSearch.vue";
+
+export default {
+  components: {
+    IconSearch,
+  },
+  props: ["isMenuOpen"],
+  data() {
+    return {};
+  },
+  computed: {
+    SearchPaddingInClose() {
+      if (this.isMenuOpen) {
+        return "px-2.5";
+      } else {
+        return "px-4";
+      }
+    },
+    SearchIconMarginInClose() {
+      if (this.isMenuOpen) {
+        return "mr-2";
+      }
+    },
+  },
+};
+</script>
