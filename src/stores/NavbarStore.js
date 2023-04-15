8ac36f93b5b4c103bca58c9fa8af0c4f4ae893a5
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-15 05:52:47 +0300

    use local storage with navbar

--- src/stores/NavbarStore.js
+++ src/stores/NavbarStore.js
@@ -1,14 +1,15 @@
 import { defineStore } from "pinia";
+import { useLocalStorage } from "@vueuse/core";
 
 export const useNavbarStore = defineStore("navbarStore", {
   state: () => ({
-    navbarMenus: [
+    navbarMenus: useLocalStorage("navbarMenus", [
       { id: 1, title: "Home", link: "/Home", isActive: true },
       { id: 2, title: "About us", link: "/About", isActive: false },
       { id: 3, title: "Lecturers", link: "/Lecturers", isActive: false },
       { id: 4, title: "Contact", link: "/Contact", isActive: false },
       { id: 5, title: "FAQs", link: "/FAQs", isActive: false },
-    ],
+    ]),
   }),
   actions: {
     setActive(id) {
@@ -16,8 +17,8 @@ export const useNavbarStore = defineStore("navbarStore", {
       if (!menu.isActive) {
         for (let i = 0; i < this.navbarMenus.length; i++) {
           this.navbarMenus[i].isActive = false;
-          menu.isActive = true;
         }
+        menu.isActive = true;
       }
     },
   },
