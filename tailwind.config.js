7d5ab7755e7135bacc5bab9c61e2f90bf9230eee
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-15 03:51:09 +0300

    update colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "light-primary-color": "#DAE6F9",
-      "light-secondary-color": "#C8DDFF",
-      "light-text-color": "#01041B",
+    extend: {
+      colors: {
+        "light-primary-color": "#DAE6F9",
+        "light-secondary-color": "#C8DDFF",
+        "light-text-color": "#01041B",
 
-      "dark-primary-color": "#01041B",
-      "dark-secondary-color": "#01062D",
-      "dark-text-color": "#DAE6F9",
-
-      white: "#ffffff",
-      transparent: "transparent",
+        "dark-primary-color": "#01041B",
+        "dark-secondary-color": "#01062D",
+        "dark-text-color": "#DAE6F9",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        openSans: ["OpenSans", "sans-serif"],
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
