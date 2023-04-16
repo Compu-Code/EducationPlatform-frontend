7eb785e4df3b8ebe3a977a0074661c8b227dcf92
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-16 05:18:12 +0300

    update sidebar / pinia

--- src/components/TheSidebar/SearchBox.vue
+++ src/components/TheSidebar/SearchBox.vue
@@ -2,7 +2,7 @@
   <section class="pl-5 pr-5">
     <div
       class="flex items-center rounded-md bg-light-primary-color mt-2 py-1 border-light-text-color duration-100"
-      :class="{ border: isMenuOpen, SearchPaddingInClose }"
+      :class="{ border: sidebarStore.isMenuOpen, SearchPaddingInClose }"
     >
       <IconSearch
         class="block float-left cursor-pointer ml-1"
@@ -14,20 +14,24 @@
         id=""
         placeholder="Search"
         class="text-base bg-transparent w-full text-light-text-color focus:outline-none"
-        :class="{ hidden: !isMenuOpen }"
+        :class="{ hidden: !sidebarStore.isMenuOpen }"
       />
     </div>
   </section>
 </template>
 
 <script>
+import { useSidebarStore } from "../../stores/SidebarStore";
 import IconSearch from "../icons/IconSearch.vue";
 
 export default {
   components: {
     IconSearch,
   },
-  inject: ["isMenuOpen"],
+  setup() {
+    const sidebarStore = useSidebarStore();
+    return { sidebarStore };
+  },
   data() {
     return {};
   },
