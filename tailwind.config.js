b5e614e5d2494fb63991d895a765749ec946f453
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-18 07:54:48 +0300

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "light-primary-color": "#DAE6F9",
+        "light-primary-color": "#F0F4FF",
         "light-secondary-color": "#C8DDFF",
         "light-text-color": "#01041B",
 
         "dark-primary-color": "#01041B",
         "dark-secondary-color": "#01062D",
-        "dark-text-color": "#DAE6F9",
+        "dark-text-color": "#F0F4FF",
+
+        "text-hover-color": "#2246CD",
+        "text-clicked-color": "#355CEF",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
