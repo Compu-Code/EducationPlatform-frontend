5c4c64303fe0f7b9234577aebe2a66b598c9ce3a
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-21 05:56:48 +0300

    colors update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,31 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // colors for light mode (BG & text)
         "light-primary-color": "#F0F4FF",
         "light-secondary-color": "#C8DDFF",
         "light-text-color": "#01041B",
 
+        // colors for dark mode (BG & text)
         "dark-primary-color": "#01041B",
         "dark-secondary-color": "#01062D",
         "dark-text-color": "#F0F4FF",
 
-        "text-hover-color": "#2246CD",
-        "text-clicked-color": "#355CEF",
+        // blue colors for light mode
+        "light-blue-normal": "#355CEF",
+        "light-blue-hover": "#2246CD",
+        "light-blue-active": "#1433AB",
+
+        // blue colors for dark mode
+        "dark-blue-normal": "#6188FF",
+        "dark-blue-hover": "#4B6FDD",
+        "dark-blue-active": "#3858BB",
+
+        /* colors for light mode hover menus */
+        "light-menu-hover": "rgba(1, 4, 27, 0.05)",
+
+        /* colors for dark mode hover menus */
+        "dark-menu-hover": "rgba(240, 244, 255, 0.05)",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
