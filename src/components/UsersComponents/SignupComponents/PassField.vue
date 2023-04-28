a3d47f904499677e39bfd0e9041453bb5cc8b715
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-04-28 06:35:18 +0300

    new png images

---
+++ src/components/UsersComponents/SignupComponents/PassField.vue
@@ -0,0 +1,81 @@
+<template>
+  <input
+    name="pass"
+    :type="[passIsHide ? 'password' : 'text']"
+    class="w-full"
+    :placeholder="[placeHolder === 1 ? 'Password' : 'Confirm Password']"
+  />
+  <div class="pass-icon absolute right-7" @click="togglePassVisibility">
+    <IconShowpass v-if="!passIsHide" />
+    <IconHidepass v-else-if="passIsHide" />
+  </div>
+</template>
+
+<script>
+import { useNavbarStore } from "../../../stores/NavbarStore";
+import IconShowpass from "../../icons/IconShowpass.vue";
+import IconHidepass from "../../icons/IconHidepass.vue";
+
+export default {
+  components: {
+    IconShowpass,
+    IconHidepass,
+  },
+  props: ["placeHolder"],
+  data() {
+    return {
+      passIsHide: true,
+    };
+  },
+  setup() {
+    const navbarStore = useNavbarStore();
+    return { navbarStore };
+  },
+  methods: {
+    togglePassVisibility() {
+      this.passIsHide = !this.passIsHide;
+    },
+  },
+  computed: {
+    changeFieldsBGColor() {
+      if (this.navbarStore.darkMode) {
+        return "rgba(240, 244, 255, 0.2)";
+      } else {
+        return "rgba(53, 92, 239, 0.1)";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user-data-form form input {
+  margin-bottom: 17px;
+  padding-left: 30px;
+  padding-top: 11.5px;
+  padding-bottom: 11.5px;
+  border: 2px solid transparent;
+  border-radius: 8px;
+  background: v-bind(changeFieldsBGColor);
+  color: #8394b1;
+  transition: all 0.1s;
+  outline: none;
+}
+.user-data-form form input:hover {
+  border: 2px solid #8394b1;
+}
+.user-data-form form input:focus {
+  border: 2px solid #b2c4e4;
+}
+.pass-icon {
+  top: calc(50% - 8.5px);
+  transform: translateY(-50%);
+  cursor: pointer;
+  /* transition: opacity 0.1s;
+  opacity: 0; */
+}
+
+/* input[name="pass"]:focus ~ .pass-icon {
+  opacity: 1;
+} */
+</style>
