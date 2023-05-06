8a2b1752e23fb4450926a105313ae6cecd216551
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-05-06 03:15:15 +0300

    edits auth pages

---
+++ src/views/UserAccess/ResetPassword.vue
@@ -0,0 +1,12 @@
+<template>
+  <ResetPass />
+</template>
+
+<script>
+import ResetPass from "../../components/UsersComponents/LoginComponents/ResetPass.vue";
+export default {
+  components: { ResetPass },
+};
+</script>
+
+<style scoped></style>
