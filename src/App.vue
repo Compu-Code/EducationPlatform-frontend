7c3886f10eb09ea267093fee6d098d35cd1bdc9a
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-05-12 15:30:32 +0300

    fix get cookie function bugs

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,27 @@
 </template>
 
 <script>
+// function to get token from cookies
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 import { useAuthStore } from "../src/stores/AuthStore";
 import { useUserStore } from "./stores/UserStore";
+import { useNavbarStore } from "./stores/NavbarStore";
+import { storeToRefs } from "pinia";
 import TheSidebar from "./components/SidebarComponents/TheSidebar.vue";
 import TheNavbar from "./components/NavbarComponents/TheNavbar.vue";
 
@@ -14,23 +33,25 @@ export default {
     TheNavbar,
   },
   setup() {
+    const navbarStore = useNavbarStore();
     const AuthStore = useAuthStore();
     const UserStore = useUserStore();
-    return { AuthStore, UserStore };
+    const { darkMode } = storeToRefs(navbarStore);
+    return { AuthStore, UserStore, navbarStore, darkMode };
   },
   methods: {
-    getCookie(name) {
-      const value = `; ${document.cookie}`;
-      const parts = value.split(`; ${name}=`);
-      if (parts.length === 2) {
-        return parts.pop().split(";").shift();
-      }
-    },
+    // getCookie(name) {
+    //   const value = `; ${document.cookie}`;
+    //   const parts = value.split(`; ${name}=`);
+    //   if (parts.length === 2) {
+    //     return parts.pop().split(";").shift();
+    //   }
+    // },
   },
   // to import token from cookies and put in AuthStore when app created
   created() {
-    console.log(this.getCookie("JWT"));
-    this.AuthStore.userToken = this.getCookie("JWT");
+    console.log(getCookie("JWT"));
+    this.AuthStore.userToken = getCookie("JWT");
     // const reg = new RegExp("\\b" + null + "\\b");
     // if (document.cookie.split("#")[1] && !reg.test(document.cookie)) {
     //   let token = document.cookie.split("#")[1];
@@ -41,8 +62,32 @@ export default {
     //   }
     // }
   },
-  mounted() {},
+  // mounted() {},
   // to get user data after reload
+  watch: {
+    darkMode(newValue, oldValue) {
+      document.body.classList.add("h-screen", "duration-300");
+      if (newValue) {
+        document.body.classList.remove("bg-light-primary-color");
+        document.body.classList.add("bg-dark-primary-color");
+      } else if (newValue === false) {
+        document.body.classList.remove("bg-dark-primary-color");
+        document.body.classList.add("bg-light-primary-color");
+      }
+    },
+  },
+  mounted() {
+    if (this.darkMode) {
+      document.body.classList.remove("bg-light-primary-color");
+      document.body.classList.add("bg-dark-primary-color");
+    } else {
+      document.body.classList.remove("bg-dark-primary-color");
+      document.body.classList.add("bg-light-primary-color");
+    }
+  },
+  unmounted() {
+    this.AuthStore.logout();
+  },
 };
 </script>
 
