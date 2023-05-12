407ff8e2797e9171ebbb41c727f59362236850ec
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-05-12 15:24:15 +0300

    create contact us components

---
+++ src/components/AdminsComponents/ContactUs/ContactusDetails.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="error-id">contactUsID : {{ contactUsID }}</div>
+    <div class="error-message">
+      test
+      <!-- {{ errorlogsStore.errorDetails.error_message }} -->
+    </div>
+    <div class="error-description">
+      test
+      <!-- {{ errorlogsStore.errorDetails.error_description }} -->
+    </div>
+    <div class="error-created-at">
+      test
+      <!-- {{ errorlogsStore.errorDetails.created_at }} -->
+    </div>
+    <div class="error-updated-at">
+      test
+      <!-- {{ errorlogsStore.errorDetails.updated_at }} -->
+    </div>
+    <div class="btns flex justify-between">
+      <router-link :to="{ name: 'admin-contact-forms' }">
+        <icon-deflated :direction="left" :border="true">Back</icon-deflated>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import { useActivitylogsStore } from "../../../stores/ActivitylogsStore";
+import { useNavbarStore } from "../../../stores/NavbarStore";
+
+const NormalDeflated = defineAsyncComponent(() =>
+  import("../../UI/BasesButton/NormalDeflated.vue")
+);
+
+const IconDeflated = defineAsyncComponent(() =>
+  import("../../UI/BasesButton/IconDeflated.vue")
+);
+
+export default {
+  props: ["contactUsID"],
+  components: {
+    NormalDeflated,
+    IconDeflated,
+  },
+  data() {
+    return {};
+  },
+  setup() {
+    const activitylogsStore = useActivitylogsStore();
+    const navbarStore = useNavbarStore();
+    return { activitylogsStore, navbarStore };
+  },
+  // mounted() {
+  //   this.errorlogsStore.showErrorData();
+  // },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/AdminsAccess/Forms/ContactusDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <ContactusDetails />
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+const ContactusDetails = defineAsyncComponent(() =>
+  import("../../../components/AdminsComponents/ContactUs/ContactusDetails.vue")
+);
+
+export default {
+  components: { ContactusDetails },
+};
+</script>
