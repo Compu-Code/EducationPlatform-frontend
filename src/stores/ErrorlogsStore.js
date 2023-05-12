7d9336e9c0918bc96dc564b5591bd2d55f059421
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-05-12 15:28:00 +0300

    create store for error logs

--- src/stores/ErrorlogsStore.js
+++ src/stores/ErrorlogsStore.js
@@ -7,67 +7,74 @@ export const useErrorlogsStore = defineStore("errorlogsStore", {
     AuthStore: useAuthStore(),
     errorLogsData: [
       {
-        id: 4,
+        id: 111,
         error_message:
           "Facere et magni omnis perferendis deleniti. Quam voluptatum accusamus nobis quod. Quod autem tempore minus quo molestiae aut commodi. Cupiditate sed aut quos et consequuntur voluptatibus dolor consequuntur. Autem nesciunt et animi magnam omnis sunt.",
         error_description:
           "Quam doloribus velit architecto eligendi eum. Alias nihil non ut tempore earum. Voluptatum ad nihil doloribus et iste.",
         created_at: "2023-04-28T14:17:48.000000Z",
         updated_at: "2023-04-28T14:17:48.000000Z",
+        isMenuActive: false,
       },
       {
-        id: 5,
+        id: 20,
+        error_message:
+          "Facere et magni omnis perferendis deleniti. Quam voluptatum accusamus nobis quod. Quod autem tempore minus quo molestiae aut commodi. Cupiditate sed aut quos et consequuntur voluptatibus dolor consequuntur. Autem nesciunt et animi magnam omnis sunt.",
+        error_description:
+          "Quam doloribus velit architecto eligendi eum. Alias nihil non ut tempore earum. Voluptatum ad nihil doloribus et iste.",
+        created_at: "2023-04-28T14:17:48.000000Z",
+        updated_at: "2023-04-28T14:17:48.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 19,
         error_message:
           "Consequuntur corporis recusandae minima ducimus. Non veniam qui quos quia iste qui animi. Error iure qui qui aperiam quia. Omnis sit officiis pariatur voluptas dicta.",
         error_description:
           "Ratione tempora ducimus aperiam officiis commodi. Voluptates nam similique nulla quas et. Perspiciatis dolorem quam fuga alias nesciunt occaecati blanditiis. Aliquid doloremque consequatur quia.",
         created_at: "2023-04-28T14:17:48.000000Z",
         updated_at: "2023-04-28T14:17:48.000000Z",
+        isMenuActive: false,
       },
       {
-        id: 6,
+        id: 18,
         error_message:
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "No query results for model [App\\Models\\ContactUs] 33",
         created_at: "2023-04-28T15:07:15.000000Z",
         updated_at: "2023-04-28T15:07:15.000000Z",
+        isMenuActive: false,
       },
       {
-        id: 10,
+        id: 17,
         error_message:
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "No query results for model [App\\Models\\OurNews] 6",
         created_at: "2023-05-06T12:36:34.000000Z",
         updated_at: "2023-05-06T12:36:34.000000Z",
+        isMenuActive: false,
       },
       {
-        id: 11,
+        id: 16,
         error_message:
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "The given role or permission should use guard `sanctum` instead of `web`.",
         created_at: "2023-05-06T17:03:21.000000Z",
         updated_at: "2023-05-06T17:03:21.000000Z",
+        isMenuActive: false,
       },
       {
-        id: 12,
+        id: 15,
         error_message:
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "The given role or permission should use guard `sanctum` instead of `web`.",
         created_at: "2023-05-07T17:42:17.000000Z",
         updated_at: "2023-05-07T17:42:17.000000Z",
-      },
-      {
-        id: 13,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "The given role or permission should use guard `sanctum` instead of `web`.",
-        created_at: "2023-05-07T17:43:55.000000Z",
-        updated_at: "2023-05-07T17:43:55.000000Z",
+        isMenuActive: false,
       },
       {
         id: 14,
@@ -75,71 +82,9 @@ export const useErrorlogsStore = defineStore("errorlogsStore", {
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "The given role or permission should use guard `sanctum` instead of `web`.",
-        created_at: "2023-05-07T17:44:42.000000Z",
-        updated_at: "2023-05-07T17:44:42.000000Z",
-      },
-      {
-        id: 15,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "The given role or permission should use guard `user` instead of `web`.",
-        created_at: "2023-05-07T17:50:34.000000Z",
-        updated_at: "2023-05-07T17:50:34.000000Z",
-      },
-      {
-        id: 4,
-        error_message:
-          "Facere et magni omnis perferendis deleniti. Quam voluptatum accusamus nobis quod. Quod autem tempore minus quo molestiae aut commodi. Cupiditate sed aut quos et consequuntur voluptatibus dolor consequuntur. Autem nesciunt et animi magnam omnis sunt.",
-        error_description:
-          "Quam doloribus velit architecto eligendi eum. Alias nihil non ut tempore earum. Voluptatum ad nihil doloribus et iste.",
-        created_at: "2023-04-28T14:17:48.000000Z",
-        updated_at: "2023-04-28T14:17:48.000000Z",
-      },
-      {
-        id: 5,
-        error_message:
-          "Consequuntur corporis recusandae minima ducimus. Non veniam qui quos quia iste qui animi. Error iure qui qui aperiam quia. Omnis sit officiis pariatur voluptas dicta.",
-        error_description:
-          "Ratione tempora ducimus aperiam officiis commodi. Voluptates nam similique nulla quas et. Perspiciatis dolorem quam fuga alias nesciunt occaecati blanditiis. Aliquid doloremque consequatur quia.",
-        created_at: "2023-04-28T14:17:48.000000Z",
-        updated_at: "2023-04-28T14:17:48.000000Z",
-      },
-      {
-        id: 6,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "No query results for model [App\\Models\\ContactUs] 33",
-        created_at: "2023-04-28T15:07:15.000000Z",
-        updated_at: "2023-04-28T15:07:15.000000Z",
-      },
-      {
-        id: 10,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "No query results for model [App\\Models\\OurNews] 6",
-        created_at: "2023-05-06T12:36:34.000000Z",
-        updated_at: "2023-05-06T12:36:34.000000Z",
-      },
-      {
-        id: 11,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "The given role or permission should use guard `sanctum` instead of `web`.",
-        created_at: "2023-05-06T17:03:21.000000Z",
-        updated_at: "2023-05-06T17:03:21.000000Z",
-      },
-      {
-        id: 12,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "The given role or permission should use guard `sanctum` instead of `web`.",
-        created_at: "2023-05-07T17:42:17.000000Z",
-        updated_at: "2023-05-07T17:42:17.000000Z",
+        created_at: "2023-05-07T17:43:55.000000Z",
+        updated_at: "2023-05-07T17:43:55.000000Z",
+        isMenuActive: false,
       },
       {
         id: 13,
@@ -147,26 +92,109 @@ export const useErrorlogsStore = defineStore("errorlogsStore", {
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "The given role or permission should use guard `sanctum` instead of `web`.",
-        created_at: "2023-05-07T17:43:55.000000Z",
-        updated_at: "2023-05-07T17:43:55.000000Z",
-      },
-      {
-        id: 14,
-        error_message:
-          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
-        error_description:
-          "The given role or permission should use guard `sanctum` instead of `web`.",
         created_at: "2023-05-07T17:44:42.000000Z",
         updated_at: "2023-05-07T17:44:42.000000Z",
+        isMenuActive: false,
       },
       {
-        id: 15,
+        id: 12,
         error_message:
           "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
         error_description:
           "The given role or permission should use guard `user` instead of `web`.",
         created_at: "2023-05-07T17:50:34.000000Z",
         updated_at: "2023-05-07T17:50:34.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 11,
+        error_message:
+          "Facere et magni omnis perferendis deleniti. Quam voluptatum accusamus nobis quod. Quod autem tempore minus quo molestiae aut commodi. Cupiditate sed aut quos et consequuntur voluptatibus dolor consequuntur. Autem nesciunt et animi magnam omnis sunt.",
+        error_description:
+          "Quam doloribus velit architecto eligendi eum. Alias nihil non ut tempore earum. Voluptatum ad nihil doloribus et iste.",
+        created_at: "2023-04-28T14:17:48.000000Z",
+        updated_at: "2023-04-28T14:17:48.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 10,
+        error_message:
+          "Consequuntur corporis recusandae minima ducimus. Non veniam qui quos quia iste qui animi. Error iure qui qui aperiam quia. Omnis sit officiis pariatur voluptas dicta.",
+        error_description:
+          "Ratione tempora ducimus aperiam officiis commodi. Voluptates nam similique nulla quas et. Perspiciatis dolorem quam fuga alias nesciunt occaecati blanditiis. Aliquid doloremque consequatur quia.",
+        created_at: "2023-04-28T14:17:48.000000Z",
+        updated_at: "2023-04-28T14:17:48.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 9,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "No query results for model [App\\Models\\ContactUs] 33",
+        created_at: "2023-04-28T15:07:15.000000Z",
+        updated_at: "2023-04-28T15:07:15.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 8,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "No query results for model [App\\Models\\OurNews] 6",
+        created_at: "2023-05-06T12:36:34.000000Z",
+        updated_at: "2023-05-06T12:36:34.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 7,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "The given role or permission should use guard `sanctum` instead of `web`.",
+        created_at: "2023-05-06T17:03:21.000000Z",
+        updated_at: "2023-05-06T17:03:21.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 6,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "The given role or permission should use guard `sanctum` instead of `web`.",
+        created_at: "2023-05-07T17:42:17.000000Z",
+        updated_at: "2023-05-07T17:42:17.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 5,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "The given role or permission should use guard `sanctum` instead of `web`.",
+        created_at: "2023-05-07T17:43:55.000000Z",
+        updated_at: "2023-05-07T17:43:55.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 4,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "The given role or permission should use guard `sanctum` instead of `web`.",
+        created_at: "2023-05-07T17:44:42.000000Z",
+        updated_at: "2023-05-07T17:44:42.000000Z",
+        isMenuActive: false,
+      },
+      {
+        id: 3,
+        error_message:
+          "C:\\xampp\\htdocs\\education_platform\\vendor\\laravel\\framework\\src\\Illuminate\\Routing\\Controller.php",
+        error_description:
+          "The given role or permission should use guard `user` instead of `web`.",
+        created_at: "2023-05-07T17:50:34.000000Z",
+        updated_at: "2023-05-07T17:50:34.000000Z",
+        isMenuActive: false,
       },
     ],
     errorDetails: null,
@@ -174,7 +202,7 @@ export const useErrorlogsStore = defineStore("errorlogsStore", {
   actions: {
     async deleteError(id) {
       const error = this.errorLogsData.find((error) => error.id === id);
-
+      console.log("delete error id:" + error.id);
       // start test
       // console.log(error.id);
       // const index = this.errorLogsData.indexOf(error);
@@ -203,23 +231,27 @@ export const useErrorlogsStore = defineStore("errorlogsStore", {
       // }
     },
     async getErrorLogsData() {
-      // try {
-      //   const response = await axios.get(
-      //     this.AuthStore.baseURL + "/api/admin/error-logs",
-      //     {
-      //       headers: {
-      //         "Access-Control-Allow-Origin": "*",
-      //         Authorization: `Bearer ${this.AuthStore.userToken}`,
-      //         "content-type": "application/json",
-      //       },
-      //     }
-      //   );
-      //   console.log(response);
-      //   console.log(response.data);
-      //   // this.errorLogsData = response.data;
-      // } catch (error) {
-      //   console.log(error);
-      // }
+      try {
+        const response = await axios.get(
+          this.AuthStore.baseURL + "/api/admin/error-logs",
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              Authorization: `Bearer ${this.AuthStore.userToken}`,
+              "content-type": "application/json",
+            },
+          }
+        );
+        console.log(response);
+        console.log(response.data);
+        // to add isMenuActive into all errorlogs to check later if popup menu open or not
+        // errorLogsData = response.data.forEach((error) => {
+        //   isMenuActive = false;
+        // });
+        // this.errorLogsData = response.data;
+      } catch (error) {
+        console.log(error);
+      }
     },
     async showErrorData(id) {
       const error = this.errorLogsData.find((error) => error.id === id);
@@ -250,6 +282,30 @@ export const useErrorlogsStore = defineStore("errorlogsStore", {
       // } catch (error) {
       //   console.log(error);
       // }
+    },
+    openMenu(ID) {
+      const error = this.errorLogsData.find((error) => error.id === ID);
+      if (error.isMenuActive) {
+        error.isMenuActive = false;
+      } else {
+        for (let i = 0; i < this.errorLogsData.length; i++) {
+          this.errorLogsData[i].isMenuActive = false;
+        }
+        error.isMenuActive = true;
+      }
+    },
+    toggleMenu(ID) {
+      const error = this.errorLogsData.find((error) => error.id === ID);
+      if (error.isMenuActive) {
+        error.isMenuActive = false;
+      } else {
+        error.isMenuActive = true;
+      }
+    },
+    closeMenus() {
+      for (let i = 0; i < this.errorLogsData.length; i++) {
+        this.errorLogsData[i].isMenuActive = false;
+      }
     },
   },
 });
