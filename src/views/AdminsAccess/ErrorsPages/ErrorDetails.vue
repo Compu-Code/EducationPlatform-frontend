4a3a4ef26a78e38fcedab28cac50375578189749
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-05-24 00:26:45 +0300

    updates on error logs

---
+++ src/views/AdminsAccess/ErrorsPages/ErrorDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <ErrorDetails />
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+const ErrorDetails = defineAsyncComponent(() =>
+  import("../../../components/AdminsComponents/ErrorLogs/ErrorDetails.vue")
+);
+
+export default {
+  components: { ErrorDetails },
+};
+</script>
+
+<style scoped></style>
