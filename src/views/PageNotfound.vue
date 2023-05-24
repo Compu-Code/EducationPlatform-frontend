11b03249fcce172227e015f7f9b8fa80b303d2fb
otba02 <79699953+Otba-02@users.noreply.github.com>
2023-05-24 10:07:15 +0300

    fix bug

--- src/views/PageNotfound.vue
+++ src/views/PageNotfound.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="page-not-found">
+  <div
+    class="page-not-found"
+    :class="
+      navbarStore.darkMode ? 'text-dark-text-color' : 'text-light-text-color'
+    "
+  >
     <h1 class="text-center text-9xl mb-10">404</h1>
     <p class="text-2xl">
       This page no longer exists or was moved to another location.
@@ -16,7 +21,14 @@
 </template>
 
 <script>
-export default {};
+import { useNavbarStore } from "../stores/NavbarStore";
+
+export default {
+  setup() {
+    const navbarStore = useNavbarStore();
+    return { navbarStore };
+  },
+};
 </script>
 
 <style scoped>
